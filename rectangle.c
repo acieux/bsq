2777fb12ef59608081c9c6c0f9472f8c7e666763
Antoine CIEUX <acieux@e3r7p15.42.fr>
2014-09-15 18:20:15 +0200

    et oui oui oui first commit maggle sisi tkt dans les ghettos de

---
+++ rectangle.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rectangle.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acieux <acieux@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/09/15 09:52:45 by acieux            #+#    #+#             */
+/*   Updated: 2014/09/15 16:31:17 by acieux           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_putchar(char c);
+
+void	which(int x, int xx, int y, int yy)
+{
+	if (x == 0 || y == 0 || x == (xx - 1) || y == (yy - 1))
+		ft_putchar('-');
+	else
+		ft_putchar('-');
+}
+
+void	rectangle(int xx, int yy)
+{
+	int		x;
+	int		y;
+
+	if (xx <= 0 || yy <= 0)
+		return ;
+	y = 0;
+	while (y < yy)
+	{
+		x = 0;
+		while (x < xx)
+		{
+			which(x, xx, y, yy);
+			x++;
+		}
+		ft_putchar('\n');
+		y++;
+	}
+}
